153152dc6b198934f4680aa286e250664d47312b
denniskxl <denniskxl@gmail.com>
2018-09-21 22:16:21 +0800

    Add files via upload

---
+++ scrapers/UN_Sanctions.py
@@ -0,0 +1,58 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import urllib
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+
+
+# In[34]:
+
+def getUnResults(name):
+    search_name1 = name.lower().replace(' ','%7C')
+    search_name2= name.lower().replace(' ','+')
+    name_lower = name.lower()
+    sites= "https://scsanctions.un.org/en/?keywords=+%22" + search_name1+ "%22&per-page=2500&sections=s&sections=s&sort=id&includes=%22"+search_name2 + "%22&excludes=&committee=&nationality=&reference_from=&reference_to="
+
+    page = urllib.request.urlopen(sites)
+    soup = BeautifulSoup(page, 'html.parser')
+
+    item_list = soup.findAll('tr', attrs={'class':'rowtext'})
+
+    for item in item_list:
+        text = item.text
+        name_index = text.find('Name: 1:')
+        if name_index != -1:
+            first_name = text[name_index + len('Name: 1:'): text.find('2:')]
+            second_name = text[text.find('2:')+ 1: text.find('3:')]
+            third_name = text[text.find('3:')+1: text.find('4:')]
+            full_name = first_name + second_name + third_name
+            full_name = full_name.replace(' na ','')
+            full_name= full_name.replace(':', '')
+            full_name = full_name.strip()
+            full_name = ' '.join(full_name.split())
+            if full_name.lower() == name_lower:
+                return name + ' FOUND in MAS Sanctions List' + " " + sites
+        else: 
+            full_name = text[text.find('Name:')+ len('Name:') : text.find('A.k.a.')]
+            full_name = full_name.replace(' na ','')
+            full_name= full_name.replace(':', '')
+            full_name = full_name.strip()
+            full_name = ' '.join(full_name.split())
+            if full_name.lower() == name_lower:
+                return name + ' FOUND in MAS Sanctions List' + " " + sites
+    return name + ' NOT FOUND in MAS Sanctions List'
+
+
+# In[35]:
+
+getUnResults("Eric Badege")
+
+
+# In[ ]:
+
+
+
